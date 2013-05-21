82b1eb1aa07315b983825546bc036556c45161af
Ismael G Marin C <ismael.marin@gmail.com>
2013-05-21 09:50:26 -0500

    Partials

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra/base'
 require 'sinatra/twitter-bootstrap'
+require 'sinatra/partial'
 
 class StartupWeekend < Sinatra::Base
   register Sinatra::Twitter::Bootstrap::Assets
+  register Sinatra::Partial
+  set :partial_template_engine, :erb
 
   get '/' do
     erb :index
